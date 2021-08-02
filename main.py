3678d730e4e3fcdf0da29297f8409243ffd31913
kasilakeconsulting <87913200+kasilakeconsulting@users.noreply.github.com>
2021-08-02 14:12:50 -0400

    Initial

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+This is sample code for fetching resulting from the National Weather Service using their API.
+
+See:
+https://weather-gov.github.io/api/general-faqs
+https://www.weather.gov/documentation/services-web-api
+'''
+
+print('PyCharm')
